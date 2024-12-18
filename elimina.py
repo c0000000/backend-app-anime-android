4239f4a8d52b0a4febdfbcf625c426b07681808b
C000000 <157703136+c0000000@users.noreply.github.com>
2024-12-19 00:41:28 +0100

    Update elimina.py

--- elimina.py
+++ elimina.py
@@ -1,19 +1,12 @@
 import json
 
-# File JSON da leggere
-ANIME_DB_FILE = 'anime_db.json'
-
-# Leggi il file JSON
-with open(ANIME_DB_FILE, 'r',errors='ignore') as file:
+# Leggi il file JSON originale
+with open('anime_db.json', 'r') as file:
     data = json.load(file)
 
-# Elimina le chiavi 'characters' e 'staff' da ogni elemento
-for item in data:
-    if 'characters' in item:
-        del item['characters']
-    if 'staff' in item:
-        del item['staff']
-
-# Riscrivi il file JSON aggiornato
-with open(ANIME_DB_FILE, 'w') as file:
-    json.dump(data, file, indent=4)
+# Seleziona solo i primi 10 oggetti
+data = data[:10]
+print(len(data))
+# Salva il nuovo file JSON
+with open('anime_db.json', 'w') as file:
+    json.dump(data, file, indent=2)
